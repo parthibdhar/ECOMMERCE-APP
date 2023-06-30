eec0d77ffff8d4c4ed1e22191c6c4ac32082af38
Parthib Dhar <parthibdhar3690@gmail.com>
2023-06-30 21:27:32 +0530

    client basic structure

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+
+function App() {
+  return (
+    < >
+    <Layout>
+      
+        <h1>Ecommerece app</h1>
+
+    </Layout>
+    </>
+  );
+}
+
+export default App;
